129fed97ee374052fbb5578999a63834bb5961ed
Evans Hauser <evanshauser@gmail.com>
2018-02-26 12:08:08 -0800

    move slack code intown file

--- src/index.js
+++ src/index.js
@@ -37,30 +37,6 @@ async function testing(robot) {
   return;
 }
 
-// const controller = Botkit.slackbot({});
-
-// const bot = controller.spawn({
-//     token: process.env.SLACK_TOKEN,
-// });
-
-// // use RTM
-// bot.startRTM(function(err,bot,payload) {
-  // console.log(err);
-  // console.log(bot);
-  // console.log(payload);
-// });
-
-// // send webhooks
-// bot.configureIncomingWebhook({url: process.env.SLACK_WEBHOOK });
-
-// bot.sendWebhook({
-  // text: 'Hey from prod!',
-// 	channel: '#apollo-outreach',
-// },function(err,res) {
-  // console.log(err);
-  // console.log(res);
-// });
-
 const possibleLabels = [
   /Feature/,
   /Bug: prod/,
